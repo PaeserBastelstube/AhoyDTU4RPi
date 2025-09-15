ffee40fa1637d14a9d1e03b6c2839e0cbcb8a977
PaeserBastelstube <plm.support@t-online.de>
2025-09-15 18:29:05 +0200

    add php semaphore

--- www/shared-memory/sysV_in.php
+++ www/shared-memory/sysV_in.php
@@ -3,8 +3,9 @@
 ################################################################################
 # System-V IPC for PHP - Semaphores, Shared Memory and Message Queues
 # https://www.php.net/manual/en/book.shmop.php
+# https://www.php.net/manual/en/book.sem.php
 ################################################################################
-date_default_timezone_set('Europe/Berlin');
+date_default_timezone_set('Europe/Berlin');    # set the default timezone to use
 
 # create any file to generate the specific FTOK key
 $filePathName = '/tmp/example';   # def a filepath / filename
@@ -16,27 +17,32 @@ fclose($fd);                      # close file-handle
 # "id" must be in chr(string) - hex(0x30) = dec(48) = chr("0") 
 $ftokKey = ftok($filePathName, "0");
 print("ftokKey=" . $ftokKey . " hex=0x" . dechex($ftokKey) . PHP_EOL);
+$sem_id = sem_get($ftokKey, 1);         # creating a semaphore
 
-$shdMemObj = shmop_open($ftokKey, "a", 0644, 0);
-if (!$shdMemObj) {
-    echo "Couldn't connect to shared memory segment\n";
-}
-// Get shared memory block's size
-$shm_size = shmop_size($shdMemObj);
-echo "SHM Block Size: " . $shm_size . " byte found.\n";
+do {
+  if (sem_acquire($sem_id)) {             # acquiring the semaphore
+    $shdMemObj = @shmop_open($ftokKey, "a", 0644, 0);
+    if (!$shdMemObj) {
+        echo "Couldn't connect to shared memory segment\n";
+        sem_remove($sem_id);            # delete a semaphore
+        break;
+    }
+    $shm_size = shmop_size($shdMemObj); # Get shared memory block's size
+    $my_string = shmop_read($shdMemObj, 0, $shm_size);  # read data back
 
-// Now lets read the string back
-$my_string = shmop_read($shdMemObj, 0, $shm_size);
-if (!$my_string) {
-    echo "Couldn't read from shared memory block\n";
-}
-echo "The data inside shared memory was: " . $my_string . "\n";
+    if (!shmop_delete($shdMemObj)) {    # delete shared memory segment
+        echo "Couldn't mark shared memory block for deletion.";
+    }
+    shmop_close($shdMemObj);            # close the shared memory segment
+    sem_release($sem_id);               # release the semaphore
 
-//Now lets delete the block and close the shared memory segment
-if (!shmop_delete($shdMemObj)) {
-    echo "Couldn't mark shared memory block for deletion.";
-}
-shmop_close($shdMemObj);
-
-echo ("To control shared memory, please call 'ipcs -m'" . PHP_EOL);
+    if (!$my_string) {
+        echo "Couldn't read from shared memory block\n";
+    } else {
+        echo "SHM Block Size: " . $shm_size . " byte found.\n";
+        echo "The data inside shared memory was: " . $my_string . "\n";
+    }
+  }
+} while (0);
+echo ("To control shared-memory and semaphore, please call 'ipcs'" . PHP_EOL);
 ?>
--- www/shared-memory/sysV_out.php
+++ www/shared-memory/sysV_out.php
@@ -3,7 +3,9 @@
 ################################################################################
 # System-V IPC for PHP - Semaphores, Shared Memory and Message Queues
 # https://www.php.net/manual/en/book.shmop.php
+# https://www.php.net/manual/en/book.sem.php
 ################################################################################
+date_default_timezone_set('Europe/Berlin'); # set the default timezone to use
 
 # create any file to generate the specific FTOK key
 $filePathName = '/tmp/example';   # def a filepath / filename
@@ -12,31 +14,33 @@ fclose($fd);                      # close file-handle
 
 # generate the specific FTOK key from file-path-name
 #  ftok(string $filename, string $project_id): int
-# "id" must be in chr(string) - hex(0x30) = dec(48) = chr("0") 
+#      "id" must be as chr(string) - hex(0x30) = dec(48) = chr("0") 
 $ftokKey = ftok($filePathName, "0");
 print("ftokKey=" . $ftokKey . " hex=0x" . dechex($ftokKey) . PHP_EOL);
+$sem_id = sem_get($ftokKey, 1);          # creating a semaphore
 
 while (1) {
-    # create TimeStamp
-    # // set the default timezone to use.
-    date_default_timezone_set('Europe/Berlin');
-    $TSnow = date('d.m.YTH:i:s');
+    $TSnow = date('d.m.YTH:i:s');        # create TimeStamp
+    if (sem_acquire($sem_id)) {          # acquiring the semaphore
+                                         # create shared memeory object
+        $shdMemObj = shmop_open($ftokKey, "c", 0644, strlen($TSnow));
+        if (!$shdMemObj) {
+            echo "Couldn't create shared memory segment\n";
+        }
 
-    # create shared memeory object
-    $shdMemObj = shmop_open($ftokKey, "c", 0644, strlen($TSnow));
-    if (!$shdMemObj) {
-        echo "Couldn't create shared memory segment\n";
-    }
+        $shm_bytes_written = shmop_write($shdMemObj, $TSnow, 0);
+        shmop_close($shdMemObj);
 
-    $shm_bytes_written = shmop_write($shdMemObj, $TSnow, 0);
-    if ($shm_bytes_written != strlen($TSnow)) {
-        echo "Couldn't write the entire length of data" . PHP_EOL;
+        if ($shm_bytes_written == strlen($TSnow))
+             print ("  len=" . $shm_bytes_written . "  TSnow=" . $TSnow . PHP_EOL);
+        else print ("  Couldn't write the entire length of data" . PHP_EOL);
+
+        sem_release($sem_id);            # release the semaphore
+        sleep(1);
     }
-    shmop_close($shdMemObj);
-    sleep(1);
-    
-    print ("  len=" . strlen($TSnow) . "  TSnow=" . $TSnow . PHP_EOL);
 }
+
+sem_remove($sem_id);                     # delete a semaphore
 
 echo ("please call 'ipcs -m' to control Shared Memory" . PHP_EOL);
 echo ("look at: 'cat /proc/sysvipc/shm'" . PHP_EOL);
