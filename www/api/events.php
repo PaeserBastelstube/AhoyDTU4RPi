91e69465e355a7126385dd4552798bcdba67a2f4
PaeserBastelstube <plm.support@t-online.de>
2026-01-02 19:19:39 +0100

    Web Serial with ahoy.log

--- www/api/events.php
+++ www/api/events.php
@@ -1,4 +1,6 @@
 <?php
+require_once'generic_json.php';
+
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 header('Connection: keep-alive');
@@ -17,23 +19,23 @@ function print2client($line){
 ob_start(); 			// Aktiviert die Ausgabepufferung
 ob_end_flush(); 		// Sendet den letzten Teil und beendet die Pufferung
 
-$ahoy_log = '/tmp/AhoyDTU_80247680.log';
+$ahoy_log = $ahoy_conf['WebServer']['filepath'] . '/AhoyDTU_' . $ahoy_conf['dtu']['serial'] . '.log';
 $nginxlog = '/var/log/nginx/access.log';
 
-$ahoy_fh = fopen($ahoy_log, 'r');	// Öffnen im Read-Modus
-$nginxfh = fopen($nginxlog, 'r');	// Öffnen im Read-Modus
+$ahoy_fh = @fopen($ahoy_log, 'r');	// Öffnen im Read-Modus
+$nginxfh = @fopen($nginxlog, 'r');	// Öffnen im Read-Modus
 
-fseek($ahoy_fh, -120, SEEK_END);	// Zum Ende der Datei springen, um nur neue Einträge zu lesen
+fseek($ahoy_fh, -121, SEEK_END);	// Zum Ende der Datei springen, um nur neue Einträge zu lesen
 fseek($nginxfh, -121, SEEK_END);	// Zum Ende der Datei springen, um nur neue Einträge zu lesen
 
 while (true) {
 	// read line an print it to client
-	while (($ahoy_line = fgets($ahoy_fh)) !== false) print2client($ahoy_line);
+	if ($ahoy_fh) while (($ahoy_line = fgets($ahoy_fh)) !== false) print2client($ahoy_line);
 	// reset file pos
 	if (filesize($ahoy_log) > ftell($ahoy_fh)) fseek($ahoy_fh, ftell($ahoy_fh));
 
 	// read line an print it to client
-	while (($nginxline = fgets($nginxfh)) !== false) print2client($nginxline);
+	if ($nginxfh) while (($nginxline = fgets($nginxfh)) !== false) print2client($nginxline);
 	// reset file pos
 	if (filesize($nginxlog) > ftell($nginxfh)) fseek($nginxfh, ftell($nginxfh));
 
